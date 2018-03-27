98dd519be3ab77266a6b279b9ba974cc98b14ea7
Darrell Payne <darrellpayne@earthlink.net>
2018-03-26 18:55:12 -0700

    Questions completed, write wrong answer triggers overlay
    At moment no way to dismiss overlay

--- lib/UI/question_text.dart
+++ lib/UI/question_text.dart
@@ -5,12 +5,27 @@ class QuestionText extends StatefulWidget {
   final String _question;
   final int _questionNumber;
 
+  QuestionText(this._question, this._questionNumber);
+
   @override
   State createState() => new QuestionTextState();
 
 }
 
-class QuestionTextState extends State<QuestionText>{
+class QuestionTextState extends State<QuestionText> with SingleTickerProviderStateMixin {
+
+  Animation<double> _fontSizeAnimation;
+  AnimationController _fontSizeAnimationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _fontSizeAnimationController = new AnimationController(duration: new Duration(milliseconds: 500), vsync: this);
+    _fontSizeAnimation = new CurvedAnimation(parent: _fontSizeAnimationController, curve: Curves.bounceOut);
+    _fontSizeAnimation.addListener(() => this.setState((){}));
+    _fontSizeAnimationController.forward();
+  }
 
   @override
   Widget build(BuildContext context){
@@ -19,7 +34,9 @@ class QuestionTextState extends State<QuestionText>{
       child: new Padding(
         padding: new EdgeInsets.symmetric(vertical: 25.0),
         child: new Center(
-          child: new Text("Statement 1: Pizza is nice"),
+          child: new Text("Question " + widget._questionNumber.toString() + " : " + widget._question,
+            style: new TextStyle(fontSize: _fontSizeAnimation.value * 15),
+          ), // Text
         ), // Center
       ),
     ); // Material
--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -3,6 +3,9 @@ import '../utils/question.dart';
 import '../utils/quiz.dart';
 import '../UI/answer_button.dart';
 import '../UI/question_text.dart';
+import '../UI/right_wrong_overlay.dart';
+
+
 
 class QuizPage extends StatefulWidget {
 
@@ -13,17 +16,50 @@ class QuizPage extends StatefulWidget {
 }
 
 class QuizPageState extends State<QuizPage>{
+
+  Question currentQuestion;
+  Quiz quiz = new Quiz([
+    new Question("Elon Musk is human", false),
+    new Question("Pizza is healthy", false),
+    new Question("Flutter is awesome", true),
+
+  ]);
+
+  String questionText;
+  int questionNumber;
+  bool isCorrect;
+  bool overlayShouldBeVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    currentQuestion = quiz.nextQuestion;
+    questionText = currentQuestion.question;
+    questionNumber = quiz.questionNumber;
+  }
+
+  void handleAnswer(bool answer){
+    isCorrect = (currentQuestion.answer == answer);
+    quiz.answer(isCorrect);
+    this.setState((){
+      overlayShouldBeVisible = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context){
     return new Stack(
+      fit: StackFit.expand,
       children: <Widget>[
         new Column( // This is our main page
           children: <Widget>[
-            new AnswerButton(true, () {print("You answered true");}),
-            new QuestionText(),
-            new AnswerButton(false, () {print("You answered false");}),
+            new AnswerButton(true, () => handleAnswer(true)),
+            new QuestionText(questionText, questionNumber),
+            new AnswerButton(false, () => handleAnswer(false)),
           ], // Widget[]
         ), // Column
+        overlayShouldBeVisible == true ? new RightWrongOverlay(isCorrect) : new Container(),
       ], // Widget[]
     ); // Stack
   } // Widget
